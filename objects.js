4da09877e30f22968394d40d1ce0787252493b1f
DANIEL JOHNSTON <danjohnston93@cox.net>
2018-02-28 22:57:51 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,4 @@
-var playlist = new Object({});
-
-playlist.Queen = 'Pressure';
+var playlist = new Object({artistName:'songTitle'});
 
 function updatePlaylist (playlist, artistName, songTitle){
   playlist.artistName = songTitle;

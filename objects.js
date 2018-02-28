62ae231d6af01addbfda7fc80d4d1bc83ca889f6
DANIEL JOHNSTON <danjohnston93@cox.net>
2018-02-28 22:48:42 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {Queen: "Pressure"}; 
+
+function updatePlaylist (playlist, artist, song){
+  playlist[artist] = song;
+  return playlist;
+}
+
+function removeFromPlaylist (playlist, artist){
+  delete.obj.artist;
+  return playlist; 
+}
